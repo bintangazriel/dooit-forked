faefba3e2085ae5269f9f5e178f0c95b53513c81
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-12-03 13:47:29 +0700

    feat: JenisTransaksi

--- kategori/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import index, add_kategori
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('add/', add_kategori, name='add_kategori'),
-]
