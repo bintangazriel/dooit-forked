bf5bb1d63da57fbf046285cb605442f07f0522ee
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2022-12-03 15:33:23 +0700

    change: integrate anggaran app with kategori app

--- anggaran/models.py
+++ anggaran/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.db.models.fields.related import ForeignKey
+from django.db.models.fields.related import ForeignKey, OneToOneField
 from users.models import CustomUser
+from kategori.models import Kategori
 
 class Anggaran(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
     nominal = models.FloatField(verbose_name="Jumlah Anggaran", null=True, blank=True)
     deskripsi = models.CharField(verbose_name="Deskripsi Anggaran", null=True, blank=True, max_length = 30)
-    # kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
-    kategori = models.CharField(verbose_name="Kategori Anggaran", null=True, blank=True, max_length = 30)
+    kategori = models.OneToOneField(Kategori, on_delete=models.CASCADE, null=True)
     tanggal_mulai = models.DateField()
     tanggal_selesai = models.DateField()
+
+    def __str__(self):
+        return self.kategori
+    
