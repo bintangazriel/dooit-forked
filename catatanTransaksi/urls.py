b46decab07bfb2d2dac8018ce4f757a83c3567b9
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2022-12-03 18:10:26 +0700

    feat: integrate catatanTransaksi with kategori app

--- catatanTransaksi/urls.py
+++ catatanTransaksi/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from catatanTransaksi.views import buat, index
+from catatanTransaksi.views import buat, index, detail, load_kategoris
 
 urlpatterns = [
     path('', index, name='index'),
     path('buat', buat, name='buat_catatan_transaksi'),
+    path('detail/<int:id>', detail, name='detail_catatan_transaksi'),
+    path('ajax/load-kategoris/', load_kategoris, name='ajax_load_kategoris'),
 ]
