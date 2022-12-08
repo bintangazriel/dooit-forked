9e11bf7c031863c06b2a487d40bafcaa06f18da8
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-12-09 01:29:44 +0700

    feat: visualize chart

--- catatanTransaksi/urls.py
+++ catatanTransaksi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from catatanTransaksi.views import buat, index, detail, load_kategoris
+from catatanTransaksi.views import buat, index, detail, load_kategoris, get_vis_laporan_keuangan, view_laporan_keuangan
 
 urlpatterns = [
     path('', index, name='index'),
     path('buat', buat, name='buat_catatan_transaksi'),
     path('detail/<int:id>', detail, name='detail_catatan_transaksi'),
     path('ajax/load-kategoris/', load_kategoris, name='ajax_load_kategoris'),
+    path('get_laporan_keuangan', get_vis_laporan_keuangan, name='get_laporan_keuangan'),
+    path('view',view_laporan_keuangan, name='view')
 ]
