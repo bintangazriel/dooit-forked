151baadb4c8e09092c95fc1261765126058e6a2d
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-12-10 12:42:15 +0700

    konsultan implementation

--- konsultan/urls.py
+++ konsultan/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard_konsultan, profil_konsultan
+from .views import dashboard_konsultan
+
+app_name = 'konsultan'
 
 urlpatterns = [
     path('dashboard/', dashboard_konsultan, name='dashboard_konsultan'),
-    path('profile/', profil_konsultan, name='profil_konsultan'),
 ]
