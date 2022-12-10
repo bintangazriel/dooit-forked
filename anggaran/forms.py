88d46e94fedb796ff1432dd763344db92e57eae7
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2022-12-10 15:41:45 +0700

    change: add form validation for date and nominal

--- anggaran/forms.py
+++ anggaran/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 from django.forms import widgets
 from .models import Anggaran
+from django.core.exceptions import ValidationError
+import datetime
 
 class AnggaranForm(forms.ModelForm):
+    def clean_tanggal_mulai(self):
+        data = self.cleaned_data['tanggal_mulai']
+
+        if data < datetime.date.today():
+            raise ValidationError("Tanggal yang dimasukkan tidak boleh lampau")
+
+        return data
+
+    def clean_tanggal_selesai(self):
+        data = self.cleaned_data['tanggal_selesai']
+
+        if data < datetime.date.today():
+            raise ValidationError("Tanggal yang dimasukkan tidak boleh lampau")
+
+        return data
+
     class Meta:
         model = Anggaran
         fields = ['nominal', 'deskripsi', 'kategori', 'tanggal_mulai', 'tanggal_selesai']
@@ -11,7 +29,8 @@ class AnggaranForm(forms.ModelForm):
                     'nominal': forms.NumberInput(
                         attrs={'class': 'form-control',
                         'placeholder': 'Masukkan nominal anggaran',
-                        'required':'true'
+                        'required':'true',
+                        'min':0
                         }),
 
                     'deskripsi': forms.TextInput(
@@ -27,7 +46,6 @@ class AnggaranForm(forms.ModelForm):
                         }),
 
                     'tanggal_mulai': forms.DateInput(
-                        # format=('%d-%m-%Y'),
                         attrs={'class': 'form-control', 
                             'placeholder': 'Pilih tanggal anggaran dimulai',
                             'type': 'date',
@@ -35,7 +53,6 @@ class AnggaranForm(forms.ModelForm):
                             }),
 
                     'tanggal_selesai': forms.DateInput(
-                        # format=('%d-%m-%Y'),
                         attrs={'class': 'form-control', 
                             'placeholder': 'Pilih tanggal anggaran selesai',
                             'type': 'date',
